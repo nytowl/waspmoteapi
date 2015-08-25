da497f2fd2fe3ba26c4ed4cee694ae1ca8255dfb
Angus Ainslie <angus@akkea.ca>
2015-08-25 11:41:17 -0600

    * make XBee debugging easier but making the UART a compile time option

--- waspmote-api/WaspXBeeCore.cpp
+++ waspmote-api/WaspXBeeCore.cpp
@@ -3201,7 +3201,7 @@ int8_t WaspXBeeCore::readXBee(uint8_t* data)
     uint8_t finishIndex=0; 
        
 	#if DEBUG_XBEE > 1
-    USB.println(F("[debug] new packet"));
+    printString("[debug] new packet\r\n", DEBUG_UART);
 	#endif
 	
 	// increment "pos" packet counter
@@ -3717,12 +3717,14 @@ uint8_t WaspXBeeCore::gen_send(const char* data)
 	gen_escaped_frame(TX, command, &length);
 
 	#if DEBUG_XBEE > 1
-	USB.print(F("[debug] TX:"));
+	printString("[debug] TX:", DEBUG_UART);
 	for(int i = 0; i < length; i++)
 	{
-		USB.printHex(TX[i]);
+    if( TX[i] < 16 )
+      printString( "0", DEBUG_UART );
+    printHex(TX[i], DEBUG_UART);
 	}
-	USB.println();
+	printString( "\r\n", DEBUG_UART );
 	#endif
 
    	// switch MUX in case SOCKET1 is used
@@ -3975,12 +3977,14 @@ int8_t WaspXBeeCore::parse_message(uint8_t* frame)
     i=1;  
     
 	#if DEBUG_XBEE > 1
-	USB.print(F("[debug] RX:"));
-    for(uint16_t i = 0; i < num_data ; i++)
+	printString("[debug] 1 RX:", DEBUG_UART);
+  for(uint16_t i = 0; i < num_data ; i++)
 	{
-		USB.printHex(memory[i]);
+    if( memory[i] < 16 )
+      printString( "0", DEBUG_UART );
+    printHex(memory[i],DEBUG_UART);
 	}
-	USB.println();
+	printString( "\r\n", DEBUG_UART );
 	#endif
 	
 	// If some corrupted frame has appeared we jump it
@@ -4397,12 +4401,14 @@ uint8_t WaspXBeeCore::txStatusResponse()
     counter3=0;
     
 	#if DEBUG_XBEE > 0
-	USB.print(F("[debug] RX:"));
+	printString("[debug] 2 RX:", DEBUG_UART);
     for(uint16_t i = 0; i < num_TX ; i++)
 	{
-		USB.printHex(ByteIN[i]);
+    if( ByteIN[i] < 16 )
+      printString( "0", DEBUG_UART );
+    printHex(ByteIN[i], DEBUG_UART);
 	}
-	USB.println();
+  printString( "\r\n", DEBUG_UART );
 	#endif
 	
     // If some corrupted frame has appeared we jump it
@@ -4651,13 +4657,15 @@ uint8_t WaspXBeeCore::txZBStatusResponse()
     counter3=0;
     
     #if DEBUG_XBEE > 0
-	USB.print(F("[debug] RX:"));
+    printString("[debug] 3 RX:", DEBUG_UART);
     for(uint16_t i = 0; i < num_TX ; i++)
-	{
-		USB.printHex(ByteIN[i]);
-	}
-	USB.println();
-	#endif
+	  {
+		  if( ByteIN[i] < 16 )
+        printString( "0", DEBUG_UART );
+      printHex(ByteIN[i], DEBUG_UART);
+    }
+    printString( "\r\n", DEBUG_UART );
+	  #endif
 	
     // If some corrupted frame has appeared we jump it
     if( ByteIN[0]!=0x7E )     
